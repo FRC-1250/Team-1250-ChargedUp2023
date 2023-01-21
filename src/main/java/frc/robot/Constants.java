6a10ccca1b1f333945cf351fcd4c75810238faa5
1250-tars <1250robotics+programming@gmail.com>
2023-01-21 12:16:14 -0500

    Elevator subsystem
    - Added PID gains for position control
    - Added solenoid and DIO for limit switch with basic methods

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,18 @@
 
 package frc.robot;
 
+import frc.robot.util.PIDGains;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -17,14 +23,16 @@ public final class Constants {
   public static class OperatorConstants {
 
     public static final int kDriverControllerPort = 0;
-   
-  }
 
-  //Elevator 
-  public static final int ElevatorSpark_CAN_ID = 9; 
-  public static final int Elevator_CAN_ID = 10;
+  }
 
   public static final int MIN_COMPRESSOR_PRESSURE = 100;
   public static final int MAX_COMPRESSOR_PRESSURE = 120;
-  
+
+  // Elevator
+  public static final PIDGains DRIVE_TALON_POSITION_ELEVATOR_GAINS = new PIDGains(0.25, 0.0, 0.0, 0.0);
+  public static final int ElevatorTalon_CAN_ID = 0;
+  public static final int kTimeoutMs = 30;
+  public static final int kPIDLoopIdx = 0;
+  public static final int LIMIT_SWITCH_PORT = 0;
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,30 +4,55 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Elevator extends SubsystemBase {
-  /** Creates a new Elevator. */
-  public Elevator() {}
-
-  CANSparkMax ElevatorSpark = new CANSparkMax(Constants.ElevatorSpark_CAN_ID,MotorType.kBrushless);
-
-  public enum ElevatorHeight{
+  public enum ElevatorHeight {
     TOP_NODE(100),
     MID_NODE(50),
-    LOW_NODE(0); 
+    LOW_NODE(0);
 
-    public final double heightInTicks; 
+    public final double heightInTicks;
 
-    ElevatorHeight(double heightInTicks){
-      this.heightInTicks = heightInTicks; 
+    ElevatorHeight(double heightInTicks) {
+      this.heightInTicks = heightInTicks;
     }
   }
 
+  private WPI_TalonFX ElevatorTalon = new WPI_TalonFX(Constants.ElevatorTalon_CAN_ID);
+
+  private Solenoid ElevatorSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 1);
+
+  private DigitalInput ElevatorLimitswitch = new DigitalInput(Constants.LIMIT_SWITCH_PORT);
+
+  /** Creates a new Elevator. */
+  public Elevator() {
+    ElevatorTalon.config_kP(0, Constants.DRIVE_TALON_POSITION_ELEVATOR_GAINS.kP, Constants.kTimeoutMs);
+    ElevatorTalon.config_kI(Constants.kPIDLoopIdx, Constants.DRIVE_TALON_POSITION_ELEVATOR_GAINS.kI, Constants.kTimeoutMs);
+    ElevatorTalon.config_kD(Constants.kPIDLoopIdx, Constants.DRIVE_TALON_POSITION_ELEVATOR_GAINS.kD, Constants.kTimeoutMs);
+  }
+
+  public void ToggleSolenoid() {
+    ElevatorSolenoid.toggle();
+  }
+
+  public void SetSolenoid(boolean value) {
+    ElevatorSolenoid.set(value);
+  }
+
+  public boolean GetLimitSwitchState() {
+    return ElevatorLimitswitch.get();
+  }
+
+  public void SetPosition(Double tickcount) {
+    ElevatorTalon.set(TalonFXControlMode.Position, tickcount);
+  }
 
   @Override
   public void periodic() {
