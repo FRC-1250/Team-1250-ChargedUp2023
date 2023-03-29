384d2c3835b3b21f6147ef9090e36c4f02d41f81
Rodney Lewis <rodneylewis95@gmail.com>
2023-03-29 17:55:30 -0400

    Update SwerveBrake.java

--- src/main/java/frc/robot/commands/Swerve/SwerveBrake.java
+++ src/main/java/frc/robot/commands/Swerve/SwerveBrake.java
@@ -6,10 +6,11 @@ package frc.robot.commands.Swerve;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
-public class SwerveBrake extends InstantCommand {
+public class SwerveBrake extends CommandBase {
   private final Drivetrain cmd_drivetrain;
   private final SwerveModuleState[] cmd_desiredStates = {
       new SwerveModuleState(0, Rotation2d.fromDegrees(135)),
@@ -17,6 +18,7 @@ public class SwerveBrake extends InstantCommand {
       new SwerveModuleState(0, Rotation2d.fromDegrees(-135)),
       new SwerveModuleState(0, Rotation2d.fromDegrees(-45))
   };
+  private final Timer timer = new Timer();
 
   public SwerveBrake(Drivetrain drivetrain) {
     cmd_drivetrain = drivetrain;
@@ -26,5 +28,22 @@ public class SwerveBrake extends InstantCommand {
   @Override
   public void initialize() {
     cmd_drivetrain.setModuleStates(cmd_desiredStates);
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public void execute() {
+    cmd_drivetrain.setModuleStates(cmd_desiredStates);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(0.2);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    cmd_drivetrain.drive(0, 0, 0, false);
   }
 }
