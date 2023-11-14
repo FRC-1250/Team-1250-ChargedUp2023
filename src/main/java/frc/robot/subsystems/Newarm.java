45ced2ec5475cfde69df49382f81c85041830cb4
1250-programming <114453660+1250-programming@users.noreply.github.com>
2023-11-14 11:14:22 -0500

    Trying to make a new arm code
    I took all the old pneumatics code from the original arm and transfered it over to this branch, and getting rid of all the talon extension parts that wouldn't be used on a new arm.

---
+++ src/main/java/frc/robot/subsystems/Newarm.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Newarm {
+    
+    private final WPI_TalonFX talon = new WPI_TalonFX(Constants.ArmCalibrations.TALON_CAN_ID);
+    private final Solenoid airBrake;
+    private final DoubleSolenoid angleToggle;
+    private final PneumaticHub pneumaticHub;
+    private boolean reverseLocked = false;
+    private Timer reverseLockTimer = new Timer();
+
+    public Newarm(PneumaticHub sPneumaticHub) {
+    pneumaticHub = sPneumaticHub;
+    angleToggle = pneumaticHub.makeDoubleSolenoid(Constants.ArmCalibrations.ANGLE_SOLENOID_FORWARD_PORT, Constants.ArmCalibrations.ANGLE_SOLENOID_REVERSE_PORT);
+    airBrake = pneumaticHub.makeSolenoid(Constants.ArmCalibrations.BRAKE_SOLENOID_PORT);
+    talon.setNeutralMode(NeutralMode.Brake);
+    talon.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    talon.setInverted(true);
+
+        
+
+    }
+    
+}
